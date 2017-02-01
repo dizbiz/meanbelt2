e035091102c09eecc2b39936d803328d91f424f1
Daniel Burstyn <danielgburstyn@gmail.com>
2017-02-01 12:31:55 -0600

    third commit

--- client/assets/app.js
+++ client/assets/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('app',['ngRoute']);
+var app = angular.module('app',['ngRoute', 'ngMessages']);
 app.config(function($routeProvider){
   $routeProvider.when('/',{
     templateUrl:"/partials/login.html"
